c426c781e0fedc535695c4c17eabd9a4dae87044
aspcodenet <stefan.holmberg@systementor.se>
2023-03-22 17:55:39 +0100

    a

--- data/dbcontext.go
+++ data/dbcontext.go
@@ -8,10 +8,12 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
+	//dsn := "root:my-secret-pw@tcp(127.0.0.1:3307)/emp?charset=utf8mb4&parseTime=True&loc=Local"
+	//DB, _ = gorm.Open(mysql.Open(dsn, &gorm.Config{}))
 	DB, _ = gorm.Open(sqlite.Open("./gorm.sqlite"), &gorm.Config{})
 	DB.AutoMigrate(&Employee{})
 	var antal int64
-	DB.Model(&Employee{}).Count(&antal)
+	DB.Model(&Employee{}).Count(&antal) // Seed
 	if antal == 0 {
 		DB.Create(&Employee{Age: 50, Namn: "Stefan", City: "Test"})
 		DB.Create(&Employee{Age: 14, Namn: "Oliver", City: "Test"})
